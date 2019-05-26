3955483d72e290c93187ade128facbdc2b7f0922
nyura123 <nyura123@gmail.com>
2019-05-26 15:36:52 -0500

    Updates

---
+++ precache-manifest.265e9dcddba4dd2afc5bc1368a8d75c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc2fcc7a6665ec59ef501bc196eac684",
+    "url": "/react-movable-block-editor/index.html"
+  },
+  {
+    "revision": "f80c4906fef8bf2f6564",
+    "url": "/react-movable-block-editor/static/css/2.dc22652a.chunk.css"
+  },
+  {
+    "revision": "7700798afb9b601b1160",
+    "url": "/react-movable-block-editor/static/css/main.8db060b8.chunk.css"
+  },
+  {
+    "revision": "f80c4906fef8bf2f6564",
+    "url": "/react-movable-block-editor/static/js/2.1fc7a988.chunk.js"
+  },
+  {
+    "revision": "7700798afb9b601b1160",
+    "url": "/react-movable-block-editor/static/js/main.cb76eeea.chunk.js"
+  },
+  {
+    "revision": "86548871e6efe405f746",
+    "url": "/react-movable-block-editor/static/js/runtime~main.913afbfa.js"
+  }
+]);
