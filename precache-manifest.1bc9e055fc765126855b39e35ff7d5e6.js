73daf79a286177488a1bbc1bd036c3b10bdccaff
nyura123 <nyura123@gmail.com>
2019-05-28 16:05:37 -0500

    Updates

---
+++ precache-manifest.1bc9e055fc765126855b39e35ff7d5e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74651cfb209212703fbe3006ee48d8db",
+    "url": "/react-movable-block-grid-example/index.html"
+  },
+  {
+    "revision": "34128be0b1382fa83f5f",
+    "url": "/react-movable-block-grid-example/static/css/2.dc22652a.chunk.css"
+  },
+  {
+    "revision": "76f6b75f5e132e29732a",
+    "url": "/react-movable-block-grid-example/static/css/main.8db060b8.chunk.css"
+  },
+  {
+    "revision": "34128be0b1382fa83f5f",
+    "url": "/react-movable-block-grid-example/static/js/2.3e3c2ba7.chunk.js"
+  },
+  {
+    "revision": "76f6b75f5e132e29732a",
+    "url": "/react-movable-block-grid-example/static/js/main.9dc2449f.chunk.js"
+  },
+  {
+    "revision": "2233938e3dc49c692bed",
+    "url": "/react-movable-block-grid-example/static/js/runtime~main.e182e9d7.js"
+  }
+]);
