6716b7d71e6019db917c54131df648bad8c7be41
nyura123 <nyura123@gmail.com>
2019-05-28 21:49:43 -0500

    Updates

---
+++ precache-manifest.2be939ff8c5073c16cda42732409ec19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f3d9aeeb06d8f9dac9cb1920a4a41bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34128be0b1382fa83f5f",
+    "url": "/static/css/2.dc22652a.chunk.css"
+  },
+  {
+    "revision": "184187dfb6a24fd0b9cd",
+    "url": "/static/css/main.8db060b8.chunk.css"
+  },
+  {
+    "revision": "34128be0b1382fa83f5f",
+    "url": "/static/js/2.3e3c2ba7.chunk.js"
+  },
+  {
+    "revision": "184187dfb6a24fd0b9cd",
+    "url": "/static/js/main.0ca7626a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-movable-block-editor/precache-manifest.c9ec5266d63c74eb47c74744bd86a4cd.js"
+  "/precache-manifest.2be939ff8c5073c16cda42732409ec19.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-movable-block-editor/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
